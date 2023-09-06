2143abe90b7600097e1df42a07ff03a663652b58
Mejor <amujobabatunde7@gmail.com>
2023-09-06 15:47:27 +0100

    feat: dynamically change the nav background onClick

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,24 +1,40 @@
+import { useState } from 'react';
 import style from '../styles/navigation.module.css';
 import { Link } from 'react-router-dom';
+
+
+
+
+interface NavItem {
+    stepNo: number;
+    navStep: string;
+    navDetails: string;
+}
+
 const Navigation = () => {
-    const navData = [
+    const [stepNum, setStepNum] = useState(1);
+    const handleStepNo = (stepNo: number) => {
+        setStepNum(prev => stepNo);
+    };
+
+    const navData: NavItem[] = [
         {
-            number: 1,
+            stepNo: 1,
             navStep: 'STEP 1',
             navDetails: 'YOUR INFO',
         },
         {
-            number: 2,
+            stepNo: 2,
             navStep: 'STEP 2',
             navDetails: 'SELECT PLAN',
         },
         {
-            number: 3,
+            stepNo: 3,
             navStep: 'STEP 3',
             navDetails: 'ADD-ONS',
         },
         {
-            number: 4,
+            stepNo: 4,
             navStep: 'STEP 4',
             navDetails: 'SUMMARY',
         },
@@ -26,15 +42,14 @@ const Navigation = () => {
     return (
         <div className={style.navigation_container}>
             {navData.map((nav) =>
-                <Link to='/' className={style.nav_child}>
-                    <p className={style.nav_num}>{nav.number}</p>
+                <Link to='/' className={style.nav_child} onClick={() => handleStepNo(nav.stepNo)}>
+                    <p className={`${style.nav_num} ${stepNum === nav.stepNo && `${style.active_step}`}`}>{nav.stepNo}</p>
                     <section>
                         <p className={style.nav_step}>{nav.navStep}</p>
                         <p className={style.nav_details}>{nav.navDetails}</p>
                     </section>
                 </Link>
             )}
-
 
         </div>
     );
