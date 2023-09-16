78c02ddebc5308fdc414009b63763f3bf2843b22
Mejor <amujobabatunde7@gmail.com>
2023-09-16 22:11:20 +0100

    feat: styled the addon card

--- src/components/Addons.tsx
+++ src/components/Addons.tsx
@@ -1,9 +1,25 @@
 import style from '../styles/addon.module.css';
+import { useState } from 'react';
 const Addons = () => {
+
+
     return (
         <div className={style.addons_container}>
             <p className={style.addons_heading}>Pick add-ons</p>
             <p className={style.addons_details}>Add-ons help enhance your gaming experience</p>
+
+
+            <div className={style.addon_card}>
+                <input type="checkbox" />
+
+                <section className={style.addon_info
+                }>
+                    <p>Online services</p>
+                    <p>Access to multiple games</p>
+                </section>
+
+                <span>$1/mo</span>
+            </div>
         </div>
     );
 };
