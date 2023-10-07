aeda0bf25f8536f3fc1f3322d4c8f5ee683192ab
Mejor <amujobabatunde7@gmail.com>
2023-10-07 19:58:42 +0100

    refactored the FooterNav component

--- src/components/FooterNav.tsx
+++ src/components/FooterNav.tsx
@@ -1,8 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import style from '../styles/navigation.module.css';
 
-
-
 interface StepComponentProps {
     userPlan: {
         selectedPlan: string[],
@@ -11,45 +9,69 @@ interface StepComponentProps {
     stepNum: number;
     setStepNum: React.Dispatch<React.SetStateAction<number>>;
 }
+
 const FooterNav = ({ stepNum, setStepNum, userPlan }: StepComponentProps) => {
-
     const navigate = useNavigate();
-    const isPlanChosen: boolean = userPlan.selectedPlan.length > 0;
-    const handleNextBtn = () => {
-        setStepNum(prev => prev + 1);
-        if (stepNum === 1) {
-            navigate('plan');
-        } else if (stepNum === 2) {
-            navigate('addons');
-        } else if (stepNum === 3) {
-            navigate('summary');
-        } else if (stepNum === 4) {
-            navigate('congratulations');
-        }
 
+    // Check if a plan has been chosen
+    const isPlanChosen: boolean = userPlan.selectedPlan.length > 0;
+
+    // Handle the "Next" button click
+    const handleNextBtn = () => {
+        setStepNum((prev) => prev + 1);
+
+        // Use a switch statement to navigate based on the current step
+        switch (stepNum) {
+            case 1:
+                navigate('plan'); // Navigate to the 'plan' route
+                break;
+            case 2:
+                navigate('addons'); // Navigate to the 'addons' route
+                break;
+            case 3:
+                navigate('summary'); // Navigate to the 'summary' route
+                break;
+            case 4:
+                navigate('congratulations'); // Navigate to the 'congratulations' route
+                break;
+            default:
+                break;
+        }
     };
+
+    // Handle the "Go Back" button click
     const handlePrevBtn = () => {
-        setStepNum(prev => prev - 1);
+        setStepNum((prev) => prev - 1);
+
+        // Use navigate(-1) to go back to the previous route
         navigate(-1);
     };
+
+    // Determine the button style based on the step
     const handleBtnStyle = (stepNo: number, ...args: number[]) => {
         return args.includes(stepNo) ? { display: 'none' } : {};
     };
 
     return (
         <div className={style.footer_nav_container}>
-            <button style={handleBtnStyle(stepNum, 1, 5)} className={style.previous_btn} onClick={handlePrevBtn}>Go Back</button>
+            {/* "Go Back" button */}
+            <button
+                style={handleBtnStyle(stepNum, 1, 5)}
+                className={style.previous_btn}
+                onClick={handlePrevBtn}
+            >
+                Go Back
+            </button>
 
+            {/* "Next Step" or "Confirm" button */}
             <button
                 className={style.next_step_btn}
                 style={handleBtnStyle(stepNum, 5)}
                 onClick={handleNextBtn}
-                disabled={stepNum === 2 && !isPlanChosen}>
+                disabled={stepNum === 2 && !isPlanChosen}
+            >
                 {stepNum !== 4 ? 'Next Step' : 'Confirm'}
-
             </button>
-            <>{console.log(stepNum)}</>
-
         </div>
     );
 };
