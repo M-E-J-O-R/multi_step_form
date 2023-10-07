793fde3264ba36f3b8400eb780ef1c766b90daa6
Mejor <amujobabatunde7@gmail.com>
2023-10-07 20:18:25 +0100

    refactored the Summary component

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -1,81 +1,91 @@
 import { Link } from 'react-router-dom';
 import style from '../styles/summary.module.css';
 
-
 type SummaryProps = {
-    isChecked: boolean,
+    isChecked: boolean;
     addonData: {
-        id: number,
-        addonHeading: string,
-        addonInfo: string,
+        id: number;
+        addonHeading: string;
+        addonInfo: string;
         addonPricing: number;
     }[];
     planData: {
-        planName: string,
-        planLogo: string,
-        planPricing: number,
-        planBonus: string,
+        planName: string;
+        planLogo: string;
+        planPricing: number;
+        planBonus: string;
     }[];
     userPlan: {
-        selectedPlan: string[],
+        selectedPlan: string[];
         selectedAddon: number[];
     };
-
 };
 
-
 const Summary = ({ userPlan, isChecked, addonData, planData }: SummaryProps) => {
-    let userSelectedAddon = addonData.filter((addons) => userPlan.selectedAddon.includes(addons.id));
-    let userSelectedPlan = planData.filter((plan) => userPlan.selectedPlan.includes(plan.planName));
+    // Filter user-selected addons and plans
+    const userSelectedAddon = addonData.filter((addon) =>
+        userPlan.selectedAddon.includes(addon.id)
+    );
+    const userSelectedPlan = planData.find((plan) =>
+        userPlan.selectedPlan.includes(plan.planName)
+    );
 
-    const totalPrice = () => {
-        let planPrice = userSelectedPlan[0]?.planPricing || 0;
-        let totalPrice = userPlan.selectedAddon.reduce((totalPrice, addonId) => {
-            const addon = addonData.find((addon) => addon.id === addonId);
-            if (addon) {
-                return totalPrice + addon.addonPricing;
-            }
-            return totalPrice;
-        }, 0);
-
-        return planPrice + totalPrice;
+    // Calculate the total price
+    const calculateTotalPrice = () => {
+        const planPrice = userSelectedPlan?.planPricing || 0;
+        const addonPrices = userSelectedAddon.reduce(
+            (total, addon) => total + addon.addonPricing,
+            0
+        );
+        return planPrice + addonPrices;
     };
 
-
-
     return (
-        <div className={style.summary_parent_container} >
+        <div className={style.summary_parent_container}>
+            {/* Summary heading and details */}
             <p className={style.summary_heading}>Finishing up</p>
-            <p className={style.summary_details}>Double check everything before confirming</p>
+            <p className={style.summary_details}>
+                Double-check everything before confirming
+            </p>
 
+            {/* User plan and selected addons */}
             <div className={style.summary_child_container}>
                 <section className={style.user_plan}>
+                    {/* User-selected plan */}
                     <div className={style.plan_child}>
-                        <p className={style.plan_heading_container}>{userSelectedPlan[0]?.planName} ({isChecked ? 'yearly' : 'monthly'})
-                            <Link to='/plan' className={style.heading} >Change</Link>
+                        <p className={style.plan_heading_container}>
+                            {userSelectedPlan?.planName} ({isChecked ? 'yearly' : 'monthly'}){' '}
+                            <Link to='/plan' className={style.heading}>
+                                Change
+                            </Link>
                         </p>
-
-                        <p className={style.plan_heading}>${userSelectedPlan[0]?.planPricing}/{isChecked ? 'yr' : 'mo'}</p>
+                        <p className={style.plan_heading}>
+                            ${userSelectedPlan?.planPricing}/{isChecked ? 'yr' : 'mo'}
+                        </p>
                     </div>
                     <hr />
-                    {userSelectedAddon.map((addon) =>
 
+                    {/* User-selected addons */}
+                    {userSelectedAddon.map((addon) => (
                         <div className={style.plan_child} key={addon.id}>
                             <p className={style.heading}>{addon.addonHeading}</p>
-                            <p className={style.pricing}>+${addon.addonPricing}/{isChecked ? 'yr' : 'mo'}</p>
+                            <p className={style.pricing}>
+                                +${addon.addonPricing}/{isChecked ? 'yr' : 'mo'}
+                            </p>
                         </div>
-                    )}
-
-
+                    ))}
                 </section>
+
+                {/* Total price */}
                 <section className={style.total_container}>
                     <p className={style.heading}>
                         Total (per {isChecked ? 'year' : 'month'})
                     </p>
-                    <p className={style.total_pricing}> +${totalPrice()}/{isChecked ? 'yr' : 'mo'}</p>
+                    <p className={style.total_pricing}>
+                        +${calculateTotalPrice()}/{isChecked ? 'yr' : 'mo'}
+                    </p>
                 </section>
             </div>
-
         </div>
     );
 };
