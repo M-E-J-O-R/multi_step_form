bc4fe89d83b2877b68a96e651540d993ead4aa5b
Mejor <amujobabatunde7@gmail.com>
2023-10-09 12:27:41 +0100

    feat: added autoComplete to the input tags

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import style from '../styles/home.module.css';
 
 type HomeParameterType = {
@@ -22,15 +21,15 @@ const Home = ({ userInfo, handleUserInfo }: HomeParameterType) => {
             <form action="" className={style.form_container}>
                 <label htmlFor="name" className={style.label_container}>
                     <p className={style.label_text}>Name</p>
-                    <input type="text" id="name" required placeholder='Vannesa Mint' onChange={(e) => handleUserInfo('name', e.target.value)} value={userInfo.name} className={style.input_field} />
+                    <input type="text" autoComplete='on' id="name" required placeholder='Vannesa Mint' onChange={(e) => handleUserInfo('name', e.target.value)} value={userInfo.name} className={style.input_field} />
                 </label>
                 <label htmlFor="email" className={style.label_container}>
                     <p className={style.label_text}>Email</p>
-                    <input type="email" id="email" required placeholder='vannesamint@' className={style.input_field} onChange={(e) => handleUserInfo('email', e.target.value)} value={userInfo.email} />
+                    <input type="email" autoComplete='on' id="email" required placeholder='vannesamint@' className={style.input_field} onChange={(e) => handleUserInfo('email', e.target.value)} value={userInfo.email} />
                 </label>
                 <label htmlFor="phone" className={style.label_container}>
                     <p className={style.label_text}>Phone</p>
-                    <input type="tel" id="phone" required placeholder='+1 234 567 890' className={style.input_field} onChange={(e) => handleUserInfo('telephone', e.target.value)} value={userInfo.telephone} />
+                    <input type="tel" autoComplete='on' id="phone" required placeholder='+1 234 567 890' className={style.input_field} onChange={(e) => handleUserInfo('telephone', e.target.value)} value={userInfo.telephone} />
                 </label>
             </form>
 
